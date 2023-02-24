bfcd63c8fc74e15d8f53f68baf0203add213f91c
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2023-02-24 12:50:58 +0600

    Tried a lot.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -8,7 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.URI;
 const client = new MongoClient(uri, {
 	useNewUrlParser: true,
@@ -16,17 +16,66 @@ const client = new MongoClient(uri, {
 	serverApi: ServerApiVersion.v1,
 });
 
+const allData = require('./Data/data.json');
+
 const run = async () => {
 	try {
 		const db = client.db('Folders');
 		const FoldersCollection = db.collection('FoldersCollection');
 		app.get('/', async (req, res) => {
-			res.send('Nested array operation is running.');
+			res.send(allData);
 		});
 		app.get('/folders', async (req, res) => {
+			let { folder, pathName } = req.body;
+			const sentLabel = folder.label;
+			const allPaths = pathName.split('%%');
+			let queryKey = '';
+			allPaths.forEach((path) => {
+				queryKey = queryKey + 'children.';
+			});
+			queryKey += 'label';
 			const query = {};
-			const result = await FoldersCollection.find(query).toArray();
-			res.send({ status: 'Success', data: result });
+			query.label = allPaths[0];
+			query[queryKey] = sentLabel;
+			const result = await FoldersCollection.findOne(query);
+			if (result) {
+				return res.send({ status: 'Failed' });
+			}
+			folder.pathName = pathName + '%%' + folder.label;
+			let updateQueryKey = '';
+			allPaths.forEach((path, index) => {
+				if (index !== allPaths.length - 1) {
+					updateQueryKey += 'children.';
+				}
+			});
+			updateQueryKey += 'label';
+
+			const updateQuery = {};
+			updateQuery.label = allPaths[0];
+			updateQuery[updateQueryKey] = allPaths[allPaths.length - 1];
+			const updateResult = FoldersCollection.updateOne(
+				{ label: allPaths[0] },
+				{
+					$addToSet: {
+						'children.$[b].children': folder,
+					},
+				},
+				{
+					arrayFilters: [{ 'b.label': allPaths[1] }],
+				}
+			);
+			res.send({
+				status: 'Success',
+				data: updateResult,
+				query: query,
+				folder: folder,
+			});
+		});
+
+		app.post('/', async (req, res) => {
+			const data = req.body;
+			const result = await FoldersCollection.insertOne(data);
+			res.send(result);
 		});
 	} finally {
 	}
